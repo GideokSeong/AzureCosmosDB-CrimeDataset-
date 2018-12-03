f4290f3b456f082565996c56df8997b9d6c67d96
GideokSeong <kinznzl33@naver.com>
2018-12-03 13:22:52 -0500

    first commit

---
+++ Phase3/Phase3/Controllers/ItemController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+using Phase3.Models;
+
+namespace Phase3.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        [ActionName("Index")]
+        public async Task<ActionResult> IndexAsync()
+        {
+            
+            var items = await DocumentDBRepository<Item>.GetItemAsync();
+            if (items == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(items);
+           
+        }
+
+        [ActionName("Create")]
+        public async Task<ActionResult> CreateAsync()
+        {
+            var items = await DocumentDBRepository<Item>.GetItemsAsync(d => !d.Completed);
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync([Bind(Include = "Id,Completed,Month,Type,Code,Location")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                await DocumentDBRepository<Item>.CreateItemAsync(item);
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+        [ActionName("Edit")]
+        public async Task<ActionResult> EditAsync(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var items = await DocumentDBRepository<Item>.GetItemAsync();
+            if (items == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(items);
+        }
+
+        [HttpPost]
+        [ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync([Bind(Include = "Id,Completed,Month,Type,Code,Location")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                await DocumentDBRepository<Item>.UpdateItemAsync(item.Id, item);
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+
+
+
+    }
+}
